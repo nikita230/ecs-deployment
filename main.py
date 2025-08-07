e40f54572781ff60ea45ad5de2cc5f34d3417c31
nikita230 <69594449+nikita230@users.noreply.github.com>
2025-08-07 11:57:53 +0530

    Updating code

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import logging
-
-from flask import Flask, request, jsonify
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World'
+    return "Hello from Flask in CI/CD!"
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
